7d98fa2b771894ede75693b953d238d3af71d1aa
wskim_inAcdm <devlunch4@gmail.com>
2021-02-04 14:10:17 +0900

    20210204.1300 spring class today fin.

--- src/main/java/kr/or/ddit/mvc/web/AjaxController.java
+++ src/main/java/kr/or/ddit/mvc/web/AjaxController.java
@@ -3,6 +3,8 @@ package kr.or.ddit.mvc.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +12,12 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
+import kr.or.ddit.user.model.UserVo;
+
 @RequestMapping("ajax")
 @Controller
 public class AjaxController {
+	private static final Logger logger = LoggerFactory.getLogger(AjaxController.class);
 
 	@ModelAttribute(name = "rangers")
 	public List<String> rangers() {
@@ -25,6 +30,18 @@ public class AjaxController {
 
 		return rangers;
 
+	}
+
+	@RequestMapping("view")
+	public String view() {
+		return "ajax/ajaxView";
+	}
+
+	@RequestMapping("form")
+	public String form(UserVo userVo) {
+		logger.debug("userVo {}", userVo);
+		// json을 표현 스프링빈을 사용
+		return "jsonView";
 	}
 
 	// localhost/ajax/joinView
@@ -40,28 +57,28 @@ public class AjaxController {
 //		return "jsonView";
 //	}
 
-	//위의 @ModelAttribute 활용으로 model에 ranger가 이미 있다.
+	// 위의 @ModelAttribute 활용으로 model에 ranger가 이미 있다.
 	// localhost/ajax/joinView
 	@RequestMapping("jsonView")
 	public String jsonView() {
 		return "jsonView";
 	}
-	
+
 	// localhost/ajax/jsonViewViewObj
 	@RequestMapping("jsonViewViewObj")
 	public View jsonViewViewObj() {
 		return new MappingJackson2JsonView();
 	}
-	
-	//과거 스타일 요즘엔 미사용 
+
+	// 과거 스타일 요즘엔 미사용
 	// localhost/ajax/jsonViewMav
 	@RequestMapping("jsonViewMav")
 	public ModelAndView jsonViewMav() {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("jsonView");
-		
+
 		return mav;
 	}
-	
-	//public void json
+
+	// public void json
 }
